e7e25092bb41780174c55668a3166b59ae0c2bce
Nabid <nabid.explore@gmail.com>
2025-02-28 20:54:19 +0600

    Implement user management actions and webhook handling for Svix integration

---
+++ database/actions/user.actions.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { connectToDatabase } from "../db";
+import User from "../models/user.model";
+import { CreateUserParams, UpdateUserParams } from "@/types/types";
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log("Error creating user", error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log("Error updating user", error);
+    throw error;
+  }
+}
+
+export async function deleteUser({ clerkId }: { clerkId: string }) {
+  try {
+    await connectToDatabase();
+
+    const existedUser = await User.findOne({ clerkId });
+    if (!existedUser) {
+      throw new Error("User not found");
+    }
+
+    await User.deleteOne({ clerkId });
+  } catch (error) {
+    console.log("Failed to delete user", error);
+    throw error;
+  }
+}
--- types/types.d.ts
+++ types/types.d.ts
@@ -5,3 +5,17 @@ export interface ICreateTodo {
   status: string;
   priority: string;
 }
+
+export interface CreateUserParams {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}
+
+export interface UpdateUserParams {
+  clerkId: string;
+  updateData: Partial<IUser>;
+  path: string;
+}
